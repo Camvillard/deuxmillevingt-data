5b78a5060fdf0cc64aaf41077738bbefb110e22c
camille villard <hello@camillevillard.com>
2019-11-26 15:53:46 -0500

    mailer style

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.welcome.subject
-  #
   def order_confirmation
     @user = params[:user]
     @order = params[:order]
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,10 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
-    UserMailer.welcome
+  def order_confirmation
+    user = User.first
+    order = Order.first
+    UserMailer.with(user: user, order: order).order_confirmation
   end
 
 end
